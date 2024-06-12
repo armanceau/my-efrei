4b8046b2633def9371e96fb33f6fd46edda7950b
armanceau <102350268+armanceau@users.noreply.github.com>
2024-06-12 11:31:04 +0200

    Update App.js

--- my-efrei/src/App.js
+++ my-efrei/src/App.js
@@ -2,20 +2,12 @@
 import './App.css';
 import {classes} from './data/data';
 import { ListeCLasse } from './data/ListeClasses';
-import { ListeEleves } from './data/ListeEleves';
 
 
 function App() {
     return (
-        <div>
-            {classes.map((value, index) => {
-                return(
-                    <div>
-                        <ListeCLasse classes={classes}/>
-                        <ListeEleves classes={classes[index].eleves}/>
-                    </div>
-                )
-            })}
+        <div>       
+            <ListeCLasse classes={classes}/>
         </div>
     );
 }
